08446ad7f2d1f6c2c0cec41bafc52b1ef0938f09
Joao Teles <joaojoseteles@gmail.com>
2023-01-27 22:30:17 -0300

    :recycle: refactor: utilizando getStaticProps e passando para component Products os arquivos vindo da api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,47 @@
-import { Footer } from "@/src/components/Footer/Footer";
-import Header from "@/src/components/Header/Header";
-import { Products } from "@/src/components/Products/Products";
+/*  Styles */
 import { Container, Main } from "@/styles/home";
 
-export default function Home() {
+/*  Components */
+import { Header } from "@/src/components/Header/Header";
+import { Products } from "@/src/components/Products/Products";
+import { Footer } from "@/src/components/Footer/Footer";
+
+/*  utilities */
+import { useGet } from "@/src/services/useGate";
+
+/*  Interfaces */
+import { IProduct } from "@/src/types/IProduct";
+
+type HomeProps = {
+  products: IProduct[];
+};
+
+export default function Home({ products }: HomeProps) {
   return (
     <>
       <Container>
         <Header />
         <Main>
-          <Products />
+          <Products products={products} />
         </Main>
         <Footer />
       </Container>
     </>
   );
 }
+
+export const getStaticProps = async () => {
+  const products = await useGet();
+  if (!products) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      products,
+    },
+    revalidate: 10,
+  };
+};
