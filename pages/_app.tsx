5f0b057eff82bf201dce96c1f9422ccdc5bb0111
Joao Teles <joaojoseteles@gmail.com>
2023-01-27 22:29:00 -0300

    :recycle: refactor: utilizando o persister do redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
-import store from "../src/store";
+import { store, persistor } from "../src/redux";
 import { GlobalStyle } from "@/styles/globals";
+import { PersistGate } from "redux-persist/integration/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <GlobalStyle />
-      <Component {...pageProps} />
+      <PersistGate loading={null} persistor={persistor}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </PersistGate>
     </Provider>
   );
 }
